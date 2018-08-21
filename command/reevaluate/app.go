55940b053718a377c9c06bddb7246a698c1c4611
Christian Winther <jippignu@gmail.com>
2018-08-21 21:00:07 +0200

    force reschedule when doing re-evaluate all
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- command/reevaluate/app.go
+++ command/reevaluate/app.go
@@ -1,6 +1,8 @@
 package reevaluate
 
 import (
+	"strings"
+
 	"github.com/hashicorp/nomad/api"
 	"github.com/seatgeek/nomad-helper/nomad"
 	log "github.com/sirupsen/logrus"
@@ -15,8 +17,13 @@ func App() error {
 	jobStubs, _, err := client.Jobs().List(&api.QueryOptions{})
 
 	for _, jobStub := range jobStubs {
+		if strings.Contains(jobStub.ID, "/periodic-") {
+			log.Info("Skipping %s - periodic job", jobStub.Name)
+			continue
+		}
+
 		log.Infof("Evaluating %s", jobStub.Name)
-		_, _, err := client.Jobs().ForceEvaluate(jobStub.ID, &api.WriteOptions{})
+		_, _, err := client.Jobs().EvaluateWithOpts(jobStub.ID, api.EvalOptions{ForceReschedule: true}, &api.WriteOptions{})
 		if err != nil {
 			log.Errorf("  %s", err)
 		}
