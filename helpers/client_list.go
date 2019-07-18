acaeb170741569303b246f7bca366f8efdcb3474
Christian Winther <jippignu@gmail.com>
2019-07-18 14:21:15 +0200

    client filtering: always print to stderr and not stdout, otherwise it may break commands like
    nomad-helper status --output-format json ... | jq "."
    
    Signed-off-by: Christian Winther <jippignu@gmail.com>

--- helpers/client_list.go
+++ helpers/client_list.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/hashicorp/nomad/api"
@@ -12,16 +13,25 @@ import (
 
 var (
 	nodeCache = make(map[string]*api.Node)
+	stderrLog *log.Logger
 )
 
+func init() {
+	// We make an logger that _always_ print to stderr to ensure CLI calls like
+	// "nomad-helper stats --output-format json | jq '.'" works. All progress and filtering
+	// output will go to stderr now instead of stdout for debug/info etc
+	stderrLog = log.New()
+	stderrLog.Out = os.Stderr
+}
+
 func FilteredClientList(client *api.Client, c *cli.Context) ([]*api.NodeListStub, error) {
-	log.Info("Finding legible nodes")
+	stderrLog.Info("Finding legible nodes")
 	nodes, _, err := client.Nodes().List(&api.QueryOptions{Prefix: c.String("filter-prefix")})
 	if err != nil {
 		return nil, err
 	}
 
-	bar := progressbar.New(len(nodes))
+	bar := progressbar.NewOptions(len(nodes), progressbar.OptionSetWriter(os.Stderr))
 	if !c.Bool("no-progress") {
 		bar.RenderBlank()
 		defer func() {
@@ -33,25 +43,25 @@ func FilteredClientList(client *api.Client, c *cli.Context) ([]*api.NodeListStub
 	for _, node := range nodes {
 		// only consider nodes that is ready
 		if node.Status != "ready" {
-			log.Debugf("Node %s is not in status=ready (%s)", node.Name, node.Status)
+			stderrLog.Debugf("Node %s is not in status=ready (%s)", node.Name, node.Status)
 			goto NEXT_NODE
 		}
 
 		// only consider nodes with the right node class
 		if class := c.String("filter-class"); class != "" && node.NodeClass != class {
-			log.Debugf("Node %s class '%s' do not match expected value '%s'", node.Name, node.NodeClass, class)
+			stderrLog.Debugf("Node %s class '%s' do not match expected value '%s'", node.Name, node.NodeClass, class)
 			goto NEXT_NODE
 		}
 
 		// only consider nodes with the right nomad version
 		if version := c.String("filter-version"); version != "" && node.Version != version {
-			log.Debugf("Node %s version '%s' do not match expected node version '%s'", node.Name, node.Version, version)
+			stderrLog.Debugf("Node %s version '%s' do not match expected node version '%s'", node.Name, node.Version, version)
 			goto NEXT_NODE
 		}
 
 		// only consider nodes with the right eligibility
 		if eligibility := c.String("filter-eligibility"); eligibility != "" && node.SchedulingEligibility != eligibility {
-			log.Debugf("Node %s eligibility '%s' do not match expected node eligibility '%s'", node.Name, node.SchedulingEligibility, eligibility)
+			stderrLog.Debugf("Node %s eligibility '%s' do not match expected node eligibility '%s'", node.Name, node.SchedulingEligibility, eligibility)
 			goto NEXT_NODE
 		}
 
@@ -66,7 +76,7 @@ func FilteredClientList(client *api.Client, c *cli.Context) ([]*api.NodeListStub
 				value := split[1]
 
 				if nodeValue := getNodeMetaProperty(node.ID, key, client); nodeValue != value {
-					log.Debugf("Node %s Meta key '%s' value '%s' do not match expected '%s'", node.Name, key, nodeValue, value)
+					stderrLog.Debugf("Node %s Meta key '%s' value '%s' do not match expected '%s'", node.Name, key, nodeValue, value)
 					goto NEXT_NODE
 				}
 			}
@@ -83,14 +93,14 @@ func FilteredClientList(client *api.Client, c *cli.Context) ([]*api.NodeListStub
 				value := split[1]
 
 				if nodeValue := getNodeAttributesProperty(node.ID, key, client); nodeValue != value {
-					log.Debugf("Node %s Attribute key '%s' value '%s' do not match expected '%s'", node.Name, key, nodeValue, value)
+					stderrLog.Debugf("Node %s Attribute key '%s' value '%s' do not match expected '%s'", node.Name, key, nodeValue, value)
 					goto NEXT_NODE
 				}
 			}
 		}
 
 		// continue to furhter processing
-		log.Debugf("Node %s passed all all filters", node.Name)
+		stderrLog.Debugf("Node %s passed all all filters", node.Name)
 		matches = append(matches, node)
 		goto NEXT_NODE
 
@@ -103,21 +113,21 @@ func FilteredClientList(client *api.Client, c *cli.Context) ([]*api.NodeListStub
 
 	if !c.Bool("no-progress") {
 		bar.Finish()
-		fmt.Println()
+		fmt.Fprintln(os.Stderr, "")
 	}
 
-	log.Infof("Found %d matched nodes", len(matches))
+	stderrLog.Infof("Found %d matched nodes", len(matches))
 
 	// only work on specific percent of nodes
 	if percent := c.Int("percent"); percent < 100 {
-		log.Infof("Only %d percent of nodes should be used", percent)
-		matches = matches[0:len(matches) * percent / 100]
+		stderrLog.Infof("Only %d percent of nodes should be used", percent)
+		matches = matches[0 : len(matches)*percent/100]
 	}
 
 	// noop mode will fail the matching to prevent any further processing
 	if c.BoolT("noop") {
 		for _, node := range matches {
-			log.Infof("Node %s matched!", node.Name)
+			stderrLog.Infof("Node %s matched!", node.Name)
 		}
 		return nil, fmt.Errorf("noop mode, aborting")
 	}
@@ -132,7 +142,7 @@ func hasFilter(c *cli.Context, field string) bool {
 func getNodeMetaProperty(nodeID string, key string, client *api.Client) string {
 	node, err := lookupNode(nodeID, client)
 	if err != nil {
-		log.Errorf("Could not lookup the node in Nomad API: %s", err)
+		stderrLog.Errorf("Could not lookup the node in Nomad API: %s", err)
 		return ""
 	}
 
@@ -147,7 +157,7 @@ func getNodeMetaProperty(nodeID string, key string, client *api.Client) string {
 func getNodeAttributesProperty(nodeID string, key string, client *api.Client) string {
 	node, err := lookupNode(nodeID, client)
 	if err != nil {
-		log.Errorf("Could not lookup the node in Nomad API: %s", err)
+		stderrLog.Errorf("Could not lookup the node in Nomad API: %s", err)
 		return ""
 	}
 
